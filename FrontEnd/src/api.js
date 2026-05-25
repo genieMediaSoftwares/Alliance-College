588088062e4b028104f58bfbf241ad45c1d6b5c6
genieMediaSoftwares <kkdigitalgrowthsoftwares@gmail.com>
2026-05-25 11:53:52 +0530

    Updated frontend and Backend and Blogs section in alliance college of hotel management

--- FrontEnd/src/api.js
+++ FrontEnd/src/api.js
@@ -1,3 +1,2 @@
-// const BASE_URL = "https://geinestudio.onrender.com";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://alliance-college.onrender.com";
 export default BASE_URL;
